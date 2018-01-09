74b920dec208ceaf5ecbd4fbe44af7cbfec26736
Ionut Dobre <idobre@developmentgateway.org>
2018-01-09 11:23:18 +0200

    use className instead of class.

--- src/scripts/components/upload/FileUpload.jsx
+++ src/scripts/components/upload/FileUpload.jsx
@@ -12,24 +12,24 @@ import ImportStore from '../../stores/ImportStore.es6';
  * Component used to upload and import a file.
  */
 class FileUpload extends Reflux.Component {
-
+  
   constructor() {
     super();
     this.store = ImportStore;
   }
-
+  
   toggleAutoGeocode() {
     Actions.invoke(Constants.ACTION_TOGGLE_AUTOGEOCODE);
   }
-
+  
   onDrop(acceptedFiles) {
     Actions.invoke(Constants.ACTION_SET_FILE, acceptedFiles);
   }
-
+  
   onRemove(name) {
     Actions.invoke(Constants.ACTION_REMOVE_FILE, name);
   }
-
+  
   onUpload() {
     if (this.state.files.length > 0) {
       Actions.invoke(Constants.ACTION_UPLOAD_FILES, this.state);
@@ -37,7 +37,7 @@ class FileUpload extends Reflux.Component {
       Actions.invoke(Constants.ACTION_UPLOAD_FILES_VALIDATION, "Please upload some files first.");
     }
   }
-
+  
   render() {
     return (
       <div className="container">
@@ -50,39 +50,39 @@ class FileUpload extends Reflux.Component {
               {
                 this.state.files.map(file =>
                   <li key={file.name}> {file.name} - {file.size} bytes
-
-
-                  {file.status === 'LOADING' ? <div className="label label-info"></div> : null}
-                      {file.status === 'LOADING' ? <div class="spinner">  <div class="rect1"></div>
-                        <div class="rect2"></div>
-                        <div class="rect3"></div>
-                        <div class="rect4"></div>
-                        <div class="rect5"></div>
-                      </div>: null}
-
-
-                      {file.status != 'LOADING'?<button onClick={(e) => this.onRemove(file.name)} className="btn btn-xs btn-default link pull-right">Remove</button> : null}
-                      {file.status === 'ERROR' ? <div className="label label-warning">Failed ({file.message})</div> : null}
-                      {file.status === 'DONE' ? <div className="label label-success">Done</div> : null}
-
+                    
+                    
+                    {file.status === 'LOADING' ? <div className="label label-info"></div> : null}
+                    {file.status === 'LOADING' ? <div className="spinner">  <div className="rect1"></div>
+                      <div className="rect2"></div>
+                      <div className="rect3"></div>
+                      <div className="rect4"></div>
+                      <div className="rect5"></div>
+                    </div>: null}
+                    
+                    
+                    {file.status != 'LOADING'?<button onClick={(e) => this.onRemove(file.name)} className="btn btn-xs btn-default link pull-right">Remove</button> : null}
+                    {file.status === 'ERROR' ? <div className="label label-warning">Failed ({file.message})</div> : null}
+                    {file.status === 'DONE' ? <div className="label label-success">Done</div> : null}
+                  
                   </li>)
               }
             </ul>
-
+            
             {this.state.error ?
               <div className="alert alert-danger" role="alert">{this.state.error}</div> : null}
           </Dropzone>
         </div>
-
+        
         <div className="upload-options">
           <Button bsSize="lg" bsStyle='success' className="pull-right" onClick={() => {this.refs.dropzone.open()}}>
             Add File
           </Button>
-
+          
           <Button bsSize="lg" bsStyle='success' className="pull-right" onClick={e => this.onUpload()}>
             Upload and Import
           </Button>
-
+          
           <div className="select-section" onClick={this.toggleAutoGeocode.bind(this)}>
             <span className={"select-box " + (this.state.autoGeocode ? "selected" : "")}></span>
             <span className="search-option-label">Auto Code Projects</span>
@@ -90,7 +90,7 @@ class FileUpload extends Reflux.Component {
         </div>
       </div>)
   }
-
+  
 }
 
 export default FileUpload;
